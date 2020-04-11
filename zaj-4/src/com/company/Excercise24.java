3700f7d4e5003d1cbb003ed6f5c180f1200b8485
Sandro Sobczyński <hastoklai@gmail.com>
2020-04-11 14:50:45 -0800

    finished homework 4

--- zaj-4/src/com/company/Excercise24.java
+++ zaj-4/src/com/company/Excercise24.java
@@ -6,14 +6,14 @@ public class Excercise24 {
 
     public static void main(String[] args) {
         int test = 371;
-        if (IsArmstrongNumber(test)) {
+        if (isArmstrongNumber(test)) {
             System.out.println(String.format("%d is a Armstrong number", test));
         } else {
             System.out.println(String.format("%d is not a Armstrong number", test));
         }
     }
 
-    private static boolean IsArmstrongNumber(int input) {
+    private static boolean isArmstrongNumber(int input) {
         char[] inputInChars = String.valueOf(input).toCharArray();
         int sum = 0;
         for (char char_ : inputInChars) {
