9c95043709614584a55c171ddb458b08654fb2ad
Sandro Sobczyński <hastoklai@gmail.com>
2020-05-12 23:23:43 +0200

    finished zaj-5 (2)

--- zaj-5/src/com/company/Excercise05_01.java
+++ zaj-5/src/com/company/Excercise05_01.java
@@ -2,28 +2,16 @@
 
 package com.company;
 
-public class Excercise24 {
+import java.util.Arrays;
+
+public class Excercise05_01 {
 
     public static void main(String[] args) {
-        int test = 371;
-        if (isArmstrongNumber(test)) {
-            System.out.println(String.format("%d is a Armstrong number", test));
-        } else {
-            System.out.println(String.format("%d is not a Armstrong number", test));
-        }
-    }
-
-    private static boolean isArmstrongNumber(int input) {
-        char[] inputInChars = String.valueOf(input).toCharArray();
-        int sum = 0;
-        for (char char_ : inputInChars) {
-            int charInInt = Integer.parseInt(String.valueOf(char_));
-            int charSum = charInInt;
-            for (int i = 0; i < inputInChars.length - 1; i++)
-                charSum *= charInInt;
-            sum += charSum;
-        }
-        return input == sum;
+        MyPoint[] points = new MyPoint[10];
+        for (int i = 0; i < points.length; i++)
+            points[i] = new MyPoint(i + 1, i + 1);
+        String pointsStrings = Arrays.toString(points);
+        System.out.println(pointsStrings);
     }
 
 }
